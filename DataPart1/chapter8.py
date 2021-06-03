35557235ed7e0f2bf4c23519b76a3c4289cb2244
정보원 <qlee0822@naver.com>
2021-06-03 10:02:41 +0900

    init commit

---
+++ DataPart1/chapter8.py
@@ -0,0 +1,47 @@
+#input()
+# 사용자 입력을 받음
+
+# a = input("Enter the Number : ")
+#
+# print(type(a))
+# print(a)
+
+#isinstance()
+#첫번째 인수 --> 인스턴스
+#두번째인수 --> 클래스 이름
+#해당 클래스의 인스턴스 여부 반환 (True, False)
+
+class Test:
+    pass
+
+a = Test()
+print(type(a))
+
+print(isinstance(a, Test))
+
+x = [1,2,3,4,5]
+print(type(x))
+
+y = (1,2,3,4,5) # list
+z = 1, 2, 3, 4, 5
+print(type(y)) # tuple
+print(type(z)) # tuple
+
+
+#sum
+#넘겨받은 리스트, 튜블의 모든 요소의 합 반환
+print(sum(x))
+print(sum(y))
+
+#zip()
+#동일개수의 자료형을 묶음
+
+a = [1, 2, 3, 4, 5]
+b = ['a', 'b','c','d','e']
+
+print(zip(a, b))
+print(list(zip(a, b)))
+
+c = ['a','b','c']
+d = [1,2,3,4]
+print(list(zip(c, d)))
